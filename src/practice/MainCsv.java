56c14477c39907f87d81e6b0c6b0ec4bb1e253db
maho-ikeda <maho.ikeda@rakus-partners.co.jp>
2021-12-10 17:07:55 +0900

    コンソールに出力できる

--- src/practice/MainCsv.java
+++ src/practice/MainCsv.java
@@ -1,5 +1,65 @@
 package practice;
 
-public class MainCsv {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class MainCsv {
+	public static void main(String[] args) {
+		  ArrayList<String[]> listAddress = new ArrayList<String[]>();
+		  ArrayList<String[]> listEnquate = new ArrayList<String[]>();
+		  ArrayList<String[]> dataList = new ArrayList<String[]>();
+		  String[] resultId;
+		  BufferedReader br = null;
+		  BufferedReader br2 = null;
+		  //String file_name = "address.csv"; // 入力ファイル
+		  try {
+		  //File file = new File(file_name);
+		  br = new BufferedReader(
+			        new InputStreamReader(new FileInputStream("address.csv"), "SHIFT_JIS"));
+		  br2 = new BufferedReader(
+			        new InputStreamReader(new FileInputStream("enquate.csv"), "SHIFT_JIS"));
+		  // readLineで一行ずつ読み込む
+		  String line; // 読み込み行
+		  String[] data; // 分割後のデータを保持する配列
+		  String[] data2;
+		  int count = 0;
+		  while ((line = br.readLine()) != null) {
+		  // lineをカンマで分割し、配列dataに設定
+		  data = line.split(",", 0);
+		  listAddress.add(data);
+		  // 1行分の読み込みデータを表示（データ間にスペース）
+//		  for (int i = 0; i < data.length; i++) {
+//		  System.out.print(listAddress);
+//		  }
+//		  System.out.println();
+		  }
+		  while ((line = br2.readLine()) != null) {
+		  // lineをカンマで分割し、配列dataに設定
+		  data2 = line.split(",", 0);
+		  listEnquate.add(data2);
+		  for(String[] item : listAddress) {
+			  for(String[] address : listEnquate) {
+				  if(item[0].equals(address[0])) {
+					System.out.println(count + " " +  item[0] + " " + item[1] + " " + item[2] + " " + address[1] + " " + address[2]);
+				  System.out.println(dataList);;
+			  }
+			count++;
+			  }
+		  }
+		  }
+		  } catch (Exception e) {
+		  System.out.println(e.getMessage());
+		  } finally {
+		  try {
+		  br.close();
+		  } catch (Exception e) {
+		  System.out.println(e.getMessage());
+		  }
+		  }
+	
+		  }
 }
+
